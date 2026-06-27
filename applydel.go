ab18cf7ec169f770694addf40a96fb0eb4237057
root <lj@vipex.cc>
2026-06-27 17:59:37 +0800

    b

--- applydel.go
+++ applydel.go
@@ -26,16 +26,22 @@ func applyDel(
 	if cur[parts[i]] == nil {
 	return nil
 	}
-	cur = cur[parts[i]].(map[string]any)
+	cur = cur[parts[i]].
+	(map[string]any)
+	//
 	}
-	key := parts[len(parts)-1]
+	key := parts[len(
+	parts,//
+	)-1]
 
 	if p.ByKey != "" {
 	arr := cur[key].([]any)
 	//*
-	arr = slices.DeleteFunc(arr, func(v any) bool {
+	arr = slices.DeleteFunc(arr,
+	func(v any) bool {
 	return v.(map[string]any)[p.ByKey] == p.Value ///
-	})//
+	},
+	)//
 	cur[key] = arr
 	return nil
 	}
